10a67bf9e25b785430d5529263c3ca98918ef074
Filippo Gentile <gentilefilippo01@gmail.com>
2023-04-09 17:51:03 +0200

    Provide Arduino Core compatible headers
    Added "WProgram.h" and "EEPROM.h" which are needed by Z21 Library
    
    Theese new headers emulate only required functionality

---
+++ EEPROM.h
@@ -0,0 +1,11 @@
+#ifndef EEPROM_H
+#define EEPROM_H
+
+/* ARDUINO COMPATIBILITY
+ *
+ * This header is provided to use Z21 Library without source modifications
+ */
+
+#include "arduinocompat.h"
+
+#endif // EEPROM_H
---
+++ WProgram.h
@@ -0,0 +1,11 @@
+#ifndef WPROGRAM_H
+#define WPROGRAM_H
+
+/* ARDUINO COMPATIBILITY
+ *
+ * This header is provided to use Z21 Library without source modifications
+ */
+
+#include "arduinocompat.h"
+
+#endif // WPROGRAM_H
